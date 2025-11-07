0e9831056c8b08a2039c4fae003564d2b4f437b6
ab9678 <abhigyanbhattacharya75@gmail.com>
2025-11-07 23:26:10 +0530

    new

---
+++ javacriptstart/oddeven.js
@@ -0,0 +1,15 @@
+
+function checkParity(){
+    let num = prompt("Number: ")
+    if (num % 2==0){
+        alert("even")
+    }
+    else{
+        alert("odd")
+    }
+    let email= document.getElementById("email").ariaValueMax;
+
+    alert(email)
+}
+
+
