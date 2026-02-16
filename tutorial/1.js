0dbf0fcd7af256239a1ba6870215fbd7da3ab103
ab9678 <abhigyanbhattacharya75@gmail.com>
2026-02-16 22:31:03 +0530

    Update

---
+++ tutorial/1.js
@@ -0,0 +1,8 @@
+let n = '88a'
+
+let covertedVal = Number(n)
+console.log(covertedVal);
+
+if (Number.isNaN(covertedVal) == true){
+    console.log("fuck you niggs")
+}
---
+++ tutorial/2.js
@@ -0,0 +1,6 @@
+const number1 = 400
+console.log(number1)
+
+
+const num = new Number(100)
+console.log(num)
